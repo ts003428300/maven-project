b4e3b4e0282c0b76862c6d2c84be8772098e6a22
Nick_jenkins <nicklai.bcl@gmail.com>
2020-11-17 12:52:31 +0800

    checkstyle

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,11 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  /**
+    * @param somerone: people name
+    * @return string
+    */
+  public final String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
